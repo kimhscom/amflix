16c0834e2dd62957396bb22d87ec6cd2709b8fea
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-07-02 23:23:38 +0900

    Add icon to the header part

--- src/Components/Fontawesome.js
+++ src/Components/Fontawesome.js
@@ -1,4 +1,4 @@
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faFilm, faTv } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faSearch);
+library.add(faSearch, faFilm, faTv);
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import styled from "styled-components";
 import logo from "../assets/logo.png";
+import "./Fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = styled.header`
   color: white;
@@ -15,6 +17,7 @@ const Header = styled.header`
   background-color: rgba(20, 20, 20, 0.8);
   z-index: 10;
   box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+  padding: 0px 10px;
 `;
 
 const Logo = styled.img`
@@ -27,9 +30,11 @@ const List = styled.ul`
 `;
 
 const Item = styled.li`
-  width: 80px;
+  width: 100px;
   height: 50px;
-  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   border-bottom: 5px solid
     ${(props) => (props.current ? "#e74c3c" : "transparent")};
   transition: border-bottom 0.5s ease-in-out;
@@ -39,8 +44,9 @@ const SLink = styled(Link)`
   height: 50px;
   font-size: 18px;
   display: flex;
-  align-items: center;
   justify-content: center;
+  align-items: center;
+  margin-left: 10px;
 `;
 
 export default withRouter(({ location: { pathname } }) => (
@@ -50,12 +56,15 @@ export default withRouter(({ location: { pathname } }) => (
     </SLink>
     <List>
       <Item current={pathname === "/"}>
+        <FontAwesomeIcon icon="film" size="2x" />
         <SLink to="/">Movies</SLink>
       </Item>
       <Item current={pathname === "/tv"}>
+        <FontAwesomeIcon icon="tv" size="2x" />
         <SLink to="/tv">TV</SLink>
       </Item>
       <Item current={pathname === "/search"}>
+        <FontAwesomeIcon icon="search" size="2x" />
         <SLink to="/search">Search</SLink>
       </Item>
     </List>
