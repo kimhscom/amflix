f055b6921ee8f03890d17c58347c661d6b866ab4
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-07-03 20:25:18 +0900

    Change movies and TV list to Carousel

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Carousel from "react-elastic-carousel";
 
 const Container = styled.div`
   :not(:last-child) {
@@ -13,16 +14,24 @@ const Title = styled.span`
   font-weight: 600;
 `;
 
-const Grid = styled.div`
-  display: flex;
-  overflow: auto;
+const CarouselColumn = styled.div`
   margin-top: 20px;
 `;
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 5, itemsToScroll: 5 },
+  { width: 1150, itemsToShow: 10, itemsToScroll: 10 },
+];
 
 const Section = ({ title, children }) => (
   <Container>
     <Title>{title}</Title>
-    <Grid>{children}</Grid>
+    <CarouselColumn>
+      <Carousel breakPoints={breakPoints} pagination={false}>
+        {children}
+      </Carousel>
+    </CarouselColumn>
   </Container>
 );
 
